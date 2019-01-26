b6fc784917116c45bfdfddcd4261d60714e1df16
Fernando <fernando_fuentes_arija@yahoo.es>
2019-01-27 00:14:07 +0100

    Avanzamos con la creacion del atributo

--- bbdd_manager/ConfigVariablesBbdd.py
+++ bbdd_manager/ConfigVariablesBbdd.py
@@ -1,6 +1,14 @@
 ##############################
-## Configuracion de la BBDD ##
+## BBDD configuration       ##
 ##############################
 server = 'localhost'
 port = 27017
 env_database = 'environmentBBDD'
+#--
+#-- Colections prefixes
+#--
+data_prefix = "data_"
+config_prefix = "conf_"
+environment_prefix = "env_"
+actions_prefix = "act_"
+
---
+++ environment_creator/config/ConfigVariablesEnv.py
@@ -0,0 +1,14 @@
+################################
+## Attribute common variables ##
+################################
+object_name = 'OBJ_NAME'
+attribute_name = 'ATT_NAME'
+attribute_constr = 'ATT_CONSTR'
+attribute_type = 'ATT_TYPE'
+################################
+## Attribute number variables ##
+################################
+num_precision = 'PRECISION'
+num_range = 'RANGE'
+num_generation = 'GENERATION'
+num_hop = 'HOP'
