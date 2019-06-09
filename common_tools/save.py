9a8938cb2949c4ca1f32534ec7c8a7cc693718af
Fernando <fernando_fuentes_arija@yahoo.es>
2019-06-09 22:32:42 +0200

    Cambiamos la funcion que guarda la curva a fichero

--- common_tools/save.py
+++ common_tools/save.py
@@ -8,7 +8,11 @@ def from_list_to_file(this_list,this_file):
     :return:
     """
     f = open(this_file,'w') # Abrimos el fichero
-    txt = ','.join(str(x) for x in this_list)
-    f.write(txt)
+    # For each elemnt
+    cont = 1
+    for x in this_list:
+        linea = this_file + ',' + str(cont) + ',' + str(x) + '\n'
+        f.write(linea)
+        cont = cont + 1
     f.close()
 
--- marketing_agent/main_marketing_agent.py
+++ marketing_agent/main_marketing_agent.py
@@ -11,18 +11,18 @@ bbdd_connec = Mongo_manager(ConfigVariablesBbdd.env_database)
 # Creamos una accion
 test_orch = Agent_orchestrator(bbdd_connec)
 # CASE 1 - 180 Iterations, 120 number of agents, 0.05 error, 0 initial_estimate
-list_of_results = test_orch.run(180,120,0.05,0)
-save.from_list_to_file(list_of_results,'save_results/test_180_120_005_0.txt')
+list_of_results = test_orch.run(180,120,0.01,2)
+save.from_list_to_file(list_of_results,'save_results/test_180_120_001_2.csv')
 x_axis = range(len(list_of_results))
 plt.plot(list_of_results)
 # CASE 2 - 180 Iterations, 120 number of agents, 0.01 error, 0 initial_estimate
-list_of_results1 = test_orch.run(180,120,0.01,0)
-save.from_list_to_file(list_of_results,'save_results/test_180_120_001_0.txt')
+list_of_results1 = test_orch.run(180,120,0.05,2)
+save.from_list_to_file(list_of_results1,'save_results/test_180_120_005_2.csv')
 x_axis1 = range(len(list_of_results1))
 plt.plot(list_of_results1)
 # CASE 3 - 180 Iterations, 120 number of agents, 0.05 error, 2 initial_estimate
-list_of_results2 = test_orch.run(180,120,0.01,2)
-save.from_list_to_file(list_of_results,'save_results/test_180_120_001_2.txt')
+list_of_results2 = test_orch.run(180,120,0.10,2)
+save.from_list_to_file(list_of_results2,'save_results/test_180_120_010_2.csv')
 x_axis2 = range(len(list_of_results2))
 plt.plot(list_of_results2)
 
